c5bfcd97df57ca7ef2c3c616821554b7a97cd5e6
Jelmergu <github@jelmerwijnja.nl>
2019-03-05 19:13:30 +0100

    Fix incorrect event definition
    Signed-off-by: Jelmergu <github@jelmerwijnja.nl>

--- src/control.lua
+++ src/control.lua
@@ -1,4 +1,4 @@
 
-script.on_event(defines.on_player_died, function(event)
+script.on_event(defines.events.on_player_died, function(event)
     game.players[event.player_index].ticks_to_respawn = 0
 end)
