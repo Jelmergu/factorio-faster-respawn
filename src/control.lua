23f5359902341fba5fe6f6a8c1bb5efcd8d0a6e2
Jelmergu <github@jelmerwijnja.nl>
2020-01-21 18:25:37 +0100

    Made timer configurable
    Signed-off-by: Jelmergu <github@jelmerwijnja.nl>

--- src/control.lua
+++ src/control.lua
@@ -1,4 +1,3 @@
-
 script.on_event(defines.events.on_player_died, function(event)
-    game.players[event.player_index].ticks_to_respawn = 0
+    game.players[event.player_index].ticks_to_respawn = settings.global['faster-respawn-timer'].value
 end)
---
+++ src/settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+    {
+        type = "int-setting",
+        name = "faster-respawn-timer",
+        setting_type = "runtime-global",
+        default_value = 30,
+        order = "a"
+    }
+})
